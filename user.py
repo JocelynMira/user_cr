6d20e4da1619ae1c3545dcf993e4367718bc3c04
JocelynMira <mkoshiba02@gmail.com>
2023-02-19 11:33:41 -1000

    finalized

--- user.py
+++ user.py
@@ -2,6 +2,8 @@ from mysqlconnection import connectToMySQL
 
 class User:
 
+    DB = "user_cr"
+
     def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
@@ -10,23 +12,51 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+    # CREATE
     @classmethod
-    #we passing data through so we add data with cls
+    # we store the data in a dictionary coming from request.form in template
+    # we passing data through so we add data with cls
     def create_user(cls, data):
         query = """
                 INSERT INTO users (first_name, last_name, email)
                 VALUES ( %(first_name)s, %(last_name)s, %(email)s)
                 """
-        return connectToMySQL('user_cr').query_db(query,data)
+        return connectToMySQL(cls.DB).query_db(query,data)
 
-
+    # READ
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('user_cr').query_db(query)
+        results = connectToMySQL(cls.DB).query_db(query)
         users = []
 
         for user in results:
+            #make object and add to list
             users.append( cls(user) )
         #this is important to return *it goes flush with for loop*
         return users
+
+    @classmethod
+    def get_one(cls, id):
+        query = """SELECT * FROM users
+                WHERE id = %(id)s; """
+        results = connectToMySQL(cls.DB).query_db(query, {"id": id})
+        #results would be in a list 
+        return cls(results[0])
+
+    # UPDATE
+    @classmethod
+    def update(cls, data):
+        query = """UPDATE users
+                SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s
+                WHERE id = %(id)s; """
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        return results
+
+    # DELETE
+    @classmethod
+    def delete(cls, id):
+        query = """DELETE FROM users 
+                WHERE id = %(id)s;"""
+        results = connectToMySQL(cls.DB).query_db(query, {'id': id})
+        return results
