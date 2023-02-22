feaaa730f4e2dac855fe5db729a348c5afcd9487
JocelynMira <112432240+JocelynMira@users.noreply.github.com>
2023-02-21 17:14:11 -1000

    Update users.py

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import render_template, redirect, request
 from flask_app import app
 from flask_app.models.user import User
 
